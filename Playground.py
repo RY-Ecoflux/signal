6f70585cc78ce72d3df95aa752c8d3568cf09d48
RY <r.yong@abmi-goroupe.com>
2023-05-22 16:10:51 +0200

    try

--- Playground.py
+++ Playground.py
@@ -1,7 +1,16 @@
 import numpy as np
 
 
-fc = 96000
-t_fin = 20 # en seconde
-t = np.arange(0, t_fin+1, 1/fc)
-print(t)
+def échelle_temps(fs, T):
+    """
+    Générer une séquence de temps
+    Entrée :
+        fs : fréquence d'échantillonnage, int
+        T : temps final, int
+    Sortie :
+        Echelle de temps, liste
+    """
+    t = np.arange(0, T+1/fs, 1/fs)
+    return t
+
+print(échelle_temps(10, 5))
