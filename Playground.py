404ecbd818ee8fac480ac86e3212f18651a2b821
RY <r.yong@abmi-goroupe.com>
2023-05-22 16:33:32 +0200

    comment

--- Playground.py
+++ Playground.py
@@ -18,17 +18,17 @@ def tracer_données(x, y):
     plt.scatter(x, y)
     plt.xticks(rotation=45)
 
-def échelle_temps(fs, T):
-    """
-    Générer une séquence de temps
-    Entrée :
-        fs : fréquence d'échantillonnage, int
-        T : temps final, int
-    Sortie :
-        Echelle de temps, liste
-    """
-    t = np.arange(0, T+1/fs, 1/fs)
-    return t
+# def échelle_temps(fs, T):
+#     """
+#     Générer une séquence de temps
+#     Entrée :
+#         fs : fréquence d'échantillonnage, int
+#         T : temps final, int
+#     Sortie :
+#         Echelle de temps, liste
+#     """
+#     t = np.arange(0, T+1/fs, 1/fs)
+#     return t
 
 try:
     tracer_données(a,b)
