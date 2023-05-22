55c4e5e5b9f2845d1b90b32dc4fe5e1494dee536
RY <r.yong@abmi-goroupe.com>
2023-05-22 16:20:25 +0200

    try exp

--- Playground.py
+++ Playground.py
@@ -1,5 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+
+a = [1, 2, 3, 4, 5]
+b = [1, 1, 1]
+
+
+def tracer_données(x, y):
+    """
+    Tracer un graphique des données choisies
+    Entrée : le temps et les données, liste
+    Sortie : graphique
+    """
+    plt.figure(1)
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.scatter(x, y)
+    plt.xticks(rotation=45)
+    plt.show()
 
 def échelle_temps(fs, T):
     """
@@ -13,4 +31,13 @@ def échelle_temps(fs, T):
     t = np.arange(0, T+1/fs, 1/fs)
     return t
 
-print(échelle_temps(10, 5))
+try:
+    plt.figure(1)
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.scatter(a, b)
+    plt.xticks(rotation=45)
+    plt.show()
+except:
+    a = a[0:len(b)]
+    tracer_données(a, b)
